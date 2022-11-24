f6b7c23fe7d3d0f98edb2943c648b0b52a335639
Hariom210799 <hariomlapshetwar@gmail.com>
2022-11-24 13:00:39 +0530

    Assignment 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import React, { Component, useState } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
+import heading from "./Heading";
+import inputQuery from "./InputQuery";
+import subHeading from "./SubHeading";
+import submitBtn from "./SubmitButton";
 
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <heading />
+      <inputQuery />
+      <subHeading />
+      <submitBtn />
     </div>
-  )
-}
-
-
+  );
+};
